65299ddd26fe025eb9ca82fd62bf4644ed672abe
aboud20-meet <aboud20@meet.mit.edu>
2019-04-11 21:37:01 +0300

    model

--- model.py
+++ model.py
@@ -6,25 +6,44 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 # Write your classes here :
-class Blog(Base):
-    __tablename__ = "blogs"
+class Product(Base):
+    __tablename__ = "products"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    author = Column(String)
-    date = Column(String)
-    short_desc = Column(String)
-    photo = Column(String)
-    long_desc = Column(String)
-    bio = Column(String)
-    pers_photo = Column(String)
-    bfile = Column(String)
+    color=Column(String)
+    picture=Column(String)
+    price=Column(Integer)
+    tag=Column(String)
+    amount_av=Column(Integer)
+    info=Column(String)
 
 
-class Hotel(Base):
-    __tablename__ = "hotels"
-    id = Column(Integer, primary_key= True)
+class Story(Base):
+    __tablename__ = "stories"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    story=Column(String)
+    name=Column(String)
+    age=Column(Integer)
+    gender=Column(String)
+    place=Column(String)
+    summery=Column(String)
+
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(Integer, primary_key=True)
     name = Column(String)
-    hotel_date = Column(String)
-    hotel_desc = Column(String)
-    price = Column(Integer)
-    hotel_photo = Column(String)
+    time=Column(String)
+    date=Column(String)
+    place=Column(String)
+    info=Column(String)
+    picture=Column(String)
+    is_coming=Column(Boolean)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String)
+    password = Column(String)
+    role = Column(Integer)
