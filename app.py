7b006a5e88ed254e8e7abfc22f943bae8e29d798
myname21-meet <myname21@meet.mit.edu>
2019-07-21 08:49:30 -0400

    removed mysql import

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 #from data import Articles
 from flask_mysqldb import MySQL
-from MySQL import *
 from MySQLdb import *
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
